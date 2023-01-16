95ffb22d70cfc652ef9ae5cee08fa29edc6d2b9d
devpark435 <devpark435@gmail.com>
2023-01-17 05:24:15 +0900

    design : classList floating button

--- lib/screens/classList.dart
+++ lib/screens/classList.dart
@@ -10,11 +10,58 @@ class ClassListScreen extends StatefulWidget {
 }
 
 class _ClassListScreenState extends State<ClassListScreen> {
+  List<String> classList = ['hi'];
+  TextEditingController groupTitleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('data'),
+      body: SafeArea(
+          child: Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: MediaQuery.of(context).size.height,
+        alignment: Alignment.topRight,
+        child: ListView.builder(
+            itemCount: classList.length == 0 ? 0 : classList.length,
+            itemBuilder: ((context, index) {
+              return Card(
+                child: Text('${classList[index]}'),
+              );
+            })),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: ((cnt) {
+              return AlertDialog(
+                title: Text('그룹생성'),
+                content: TextFormField(
+                  controller: groupTitleController,
+                  cursorColor: Colors.green,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1, color: Colors.grey)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1, color: Colors.grey)),
+                    hintText: "그룹명",
+                    focusColor: Colors.grey,
+                    border: InputBorder.none,
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                      onPressed: (() {
+                        classList.add(groupTitleController.text);
+                        ClassListScreen();
+                      }),
+                      child: Text('data'))
+                ],
+              );
+            }),
+          );
+        },
+        child: Icon(Icons.group),
+        backgroundColor: Colors.grey,
       ),
     );
   }
